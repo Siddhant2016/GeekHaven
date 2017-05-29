a8a0cd6e232e5740b08a6424b83039c9cde91407
Siddhant2016 <harshsrivastav123@gmail.com>
2017-05-29 09:35:26 +0530

    Added a slider

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-$(document).on('click', 'a', function (event) {
+$(document).on('click', 'a.p', function (event) {
 	event.preventDefault();
 	$('html, body').animate({
 		scrollTop: $($.attr(this,'href')).offset().top
 	}, 500);
 });
+
+
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
