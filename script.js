49895c99b58b82530231cb0094c735d665cba476
Siddhant2016 <harshsrivastav123@gmail.com>
2017-05-31 11:31:25 +0530

    Added transitions between the pages

--- script.js
+++ script.js
@@ -5,6 +5,27 @@ $(document).on('click', 'a.p', function (event) {
 	}, 500);
 });
 
+$(document).ready(function() {
+	$('.thar-three').click(function(){
+			$('#main-heading').hide(500);
+			$('#holder').hide(500);
+			$('#intro').hide(500);
+			var href = $(this).attr('href');
+			setTimeout(function(){window.location = href}, 800);
+			return false;
+	});		
+});
+
+$(document).ready(function(){
+	$('#banner-text').show(500);
+	$('#holder').show(500);
+	$('#heading').show(500);
+	$('#slider').show(500);
+	$('#up').show(500);
+	$('#platform').show(500);
+	$('#main-heading').show(500);
+	$('#intro').show(500);
+});
 
 
 function openNav() {
